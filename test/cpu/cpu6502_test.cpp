507b557879168e10d67f2afdf3ba7880634651a8
John Magdaleno <johnamagdaleno@gmail.com>
2026-08-07 10:32:40 -0400

    test: cover CPU6502::reset() power-on state

--- test/cpu/cpu6502_test.cpp
+++ test/cpu/cpu6502_test.cpp
@@ -5,3 +5,17 @@
 TEST(CPU6502Smoke, GTestWiringWorks) {
     EXPECT_TRUE(true);
 }
+
+TEST(CPU6502Reset, SetsRegistersAndFlagsToPowerOnState) {
+    CPU6502 cpu;
+    cpu.reset();
+
+    EXPECT_EQ(cpu.A(), 0);
+    EXPECT_EQ(cpu.X(), 0);
+    EXPECT_EQ(cpu.Y(), 0);
+    EXPECT_EQ(cpu.PC(), 0);
+    EXPECT_EQ(cpu.SP(), 0xFF);
+    EXPECT_TRUE(cpu.IFlag());
+    EXPECT_FALSE(cpu.DFlag());
+    EXPECT_TRUE(cpu.BFlag());
+}
